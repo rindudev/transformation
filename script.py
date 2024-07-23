70e4a8b4088cd62f5d1a84f803a8595ec9e1f1a4
rindudev <rindulekha98@gmail.com>
2024-07-23 11:50:54 -0400

    2nd transformation

--- script.py
+++ script.py
@@ -10,6 +10,26 @@ print(del_work.head())
 del_work['Period'] = del_work['Period'].astype(str) 
 #  you need split column name period
 del_work['Period_year'] = del_work['Period'].str.split('.').str[0]
-
 print(del_work[['Period', 'Period_year', 'Series_reference', 'Data_value']])
 
+
+del_work1 = del_work.groupby(['Series_reference', 'Period_year'])['Data_value'].max()
+print(del_work1)
+
+mapping_dict = {}
+for i , v in del_work1.items():
+    
+    if i[0] in mapping_dict.keys():
+        if v > mapping_dict[i[0]]['Data_value'] :
+            mapping_dict[i[0]]['Period'] = i[1]
+            mapping_dict[i[0]]['Data_value'] = v
+    else :
+        mapping_dict[i[0]] = {}
+        mapping_dict[i[0]]['Period'] =  i[1] 
+        mapping_dict[i[0]]['Data_value'] = v
+        
+# convert dictionary in to data frame
+max_sale_year = pd.DataFrame.from_dict(mapping_dict, orient='index', columns=['Period', 'Data_value'])
+
+print(max_sale_year.describe)
+    
